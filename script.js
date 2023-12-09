66558bb4ef02c20a36092f2ea4769b82e13077cd
Valtece <filhovaltece@gmail.com>
2023-12-09 15:30:18 -0300

    Publicação Projeto

---
+++ script.js
@@ -0,0 +1,6 @@
+function salvarDados() {
+  var inputName = document.getElementById("inputNome").value;
+  var inputEmail = document.getElementById("inputE-mail");
+
+  visualizacao.textContent = inputName ;
+}
